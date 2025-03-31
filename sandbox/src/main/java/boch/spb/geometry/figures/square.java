b291f95c99dd047d6737e5646c3aab1c613a8f05
Мария <irktel@gmail.com>
2025-03-31 20:47:01 +0300

    выполнена структуризация кода

--- sandbox/src/main/java/boch/spb/geometry/figures/square.java
+++ sandbox/src/main/java/boch/spb/geometry/figures/square.java
@@ -1,7 +1,9 @@
 package boch.spb.geometry.figures;
 
 public class square {
-    public static void printSquareArea(double side) {System.out.println("Площадь квадрата со стороной " + side + " равен " + squareArea(side));
+    public static void printSquareArea(double side) {
+        String text = String.format("Площадь квадрата со стороной %f = %f", side, squareArea(side));
+        System.out.println(text);
     }
 
     private static double squareArea(double a) {
